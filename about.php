ff9a723f1b221cee6461a432180768dffb78b1d5
Vikas A <vikasfeb8@gmail.com>
2025-12-03 21:11:59 -0800

    about file

---
+++ about.php
@@ -0,0 +1,16 @@
+<div style="margin:0 auto;"> <br />
+    <fieldset style="padding: 10px; border: 2px solid #000;">
+        <legend>Model Testing Modified</legend>
+        <div style="overflow: hidden; padding: 10px;">
+            <div id='credits'>
+                <b>Template Plugin Developed By:</b><br />
+                <br />
+                Vik0Asth<br />
+                <br />
+                <a href='https://github.com/vik0asth/fpp-model-mod-testing'>Git Repository</a><br>
+                <a href='https://github.com/vik0asth/fpp-model-mod-testing/issues'>Bug Reporter</a><br>
+                <br />
+            </div>
+        </div>
+    </fieldset>
+</div>
